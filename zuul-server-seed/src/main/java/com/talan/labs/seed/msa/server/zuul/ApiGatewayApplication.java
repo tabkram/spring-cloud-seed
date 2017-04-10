21e8195d0f375b124faec3917af61d847dcca9a0
Akram Tabka <tabkram@gmail.com>
2017-04-11 00:07:12 +0200

    Feat (zuul) : change timeout for heartbeat sleeping

--- zuul-server-seed/src/main/java/com/talan/labs/seed/msa/server/zuul/ApiGatewayApplication.java
+++ zuul-server-seed/src/main/java/com/talan/labs/seed/msa/server/zuul/ApiGatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableHystrix
 @EnableHystrixDashboard
 public class ApiGatewayApplication {
 
